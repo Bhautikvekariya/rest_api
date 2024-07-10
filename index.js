c965f69ca6d8d0f26547199dcd1e7e3158749889
BhautikVekariya <vbhautik41@gmail.com>
2024-07-10 11:45:00 +0530

    quora crud

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app=express();
 const path=require("path");
 let port=8080;
 const { v4 :uuidv4} = require('uuid');
+const methodOverride = require('method-override');
 
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 
@@ -50,14 +52,28 @@ app.get("/posts/:id",(req,res)=>{
     res.render("show.ejs",{post});
 });
 
+app.delete("/posts/:id",(req,res)=>{
+    let {id}=req.params;
+      posts=posts.filter((p)=> id!==p.id);
+    res.redirect("/posts");
+});
+
 app.patch("/posts/:id",(req,res)=>{
     let {id}=req.params;
     let newcontent=req.body.content;
     let post=posts.find((p)=> id===p.id);
         post.content=newcontent;
         console.log(post);
-    res.send("patch request was working");
+    res.redirect("/posts");
 });
+
+
+app.get("/posts/:id/edit",(req,res)=>{
+    let {id}=req.params;
+    let post=posts.find((p)=> id!==p.id);
+    res.render("edit.ejs",{post});
+})
+
 
 app.listen(port,()=>{
     console.log(`server is running to port: 8080 `);
